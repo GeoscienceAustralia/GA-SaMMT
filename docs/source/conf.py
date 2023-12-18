ca559961c77be2322ff86a8c78d62208ecc9a4ac
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2023-12-18 13:03:35 +1100

    Update configuration to support bibtex styling.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,12 +13,16 @@ author = 'Geoscience Australia'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinxcontrib.bibtex",
+]
+bibtex_bibfiles = ['references.bib']
+bibtex_reference_style = "author_year"
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+# latex_logo = "images/test-image.png"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
