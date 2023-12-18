f9abb3366207652351c41bd3d4a946d973cad060
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2023-12-18 16:30:34 +1100

    Change theme for html outputs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'GA-SaMMT'
-copyright = '2023, Geoscience Australia'
-author = 'Geoscience Australia'
+project = "Geoscience Australia's Semi-automated Morphological Mapping Tools (GA-SaMMT)"
+copyright = "2023, Geoscience Australia"
+author = "Marine and Coastal Geoscience Team"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -16,16 +16,17 @@ author = 'Geoscience Australia'
 extensions = [
     "sphinxcontrib.bibtex",
 ]
-bibtex_bibfiles = ['references.bib']
+bibtex_bibfiles = ["references.bib"]
 bibtex_reference_style = "author_year"
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
+# -- Options for LaTeX output -------------------------------------------------
 # latex_logo = "images/test-image.png"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "nature"
+html_static_path = ["_static"]
